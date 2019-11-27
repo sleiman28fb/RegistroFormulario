b3378690f322a47af84fd84f3d6186754a57a562
sleiman28fb <55219264+sleiman28fb@users.noreply.github.com>
2019-11-27 08:20:35 -0600

    cambio de logo

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig : {
+    apiKey: "AIzaSyBPBpkJeMM9DkgVpx09gjAA5x98mvZ8nJM",
+    authDomain: "formulario-registro-f7d9d.firebaseapp.com",
+    databaseURL: "https://formulario-registro-f7d9d.firebaseio.com",
+    projectId: "formulario-registro-f7d9d",
+    storageBucket: "formulario-registro-f7d9d.appspot.com",
+    messagingSenderId: "47830451555",
+    appId: "1:47830451555:web:ed21295e2da0613a3dbf56",
+    measurementId: "G-ELEJ173JCN"
+  }
 };
